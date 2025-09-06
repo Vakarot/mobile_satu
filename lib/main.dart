657cd3a8b85b63cc4d18fcf399efd0fd26e3cfe9
Vakarot <luthfank345@gmail.com>
2025-09-06 14:35:56 +0700

    add login_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Aplikasi Kalkulator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LoginPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
